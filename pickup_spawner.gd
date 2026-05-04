7c5d5386e3f4d53bfd8b5a859374129c493a1c8d
strawberry-cow38 <growtopiahelperofficial@gmail.com>
2026-05-04 00:09:35 +0000

    spawner: drop hardcoded fruit spawns; rely on editor item-spawn tables

--- pickup_spawner.gd
+++ pickup_spawner.gd
@@ -3,22 +3,7 @@ extends Node3D
 const Pickup = preload("res://pickup.gd")
 
 # (item_id, count, position)
-const SPAWNS: Array = [
-	["apple",      1, Vector3( 4.0, 0.3,  3.0)],
-	["apple",      1, Vector3( 6.5, 0.3,  3.5)],
-	["banana",     1, Vector3(-3.0, 0.3,  2.0)],
-	["banana",     1, Vector3(-3.8, 0.3,  4.5)],
-	["orange",     1, Vector3( 8.0, 0.3, -4.0)],
-	["grape",      1, Vector3(-6.0, 0.3, -1.0)],
-	["grape",      1, Vector3(-7.2, 0.3, -2.5)],
-	["grape",      1, Vector3(-8.0, 0.3, -1.5)],
-	["lemon",      1, Vector3( 2.0, 0.3, -7.0)],
-	["strawberry", 1, Vector3( 0.0, 0.3,  9.0)],
-	["strawberry", 1, Vector3( 1.2, 0.3,  9.5)],
-	["pineapple",  1, Vector3(-9.0, 0.4,  6.0)],
-	["watermelon", 1, Vector3(10.0, 0.5, -8.0)],
-	["watermelon", 1, Vector3(12.0, 0.5,  6.0)],
-]
+const SPAWNS: Array = []
 
 func _ready() -> void:
 	for entry in SPAWNS:
